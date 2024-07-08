29eee5f9c5f1d9844c02ba5f69a6218b30c690e4
BARNABAS YI JIE <barnabas.soh.yi.jie@ibm.com>
2024-07-08 11:44:05 +0800

    Update redirectUri for okta sign in

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
     oidc: {
         clientId: '0oai2dzwta1JeNeoV5d7',
         issuer: 'https://dev-83258750.okta.com/oauth2/default',
-        redirectUri: 'https://localhost:4200/login/callback',
+        redirectUri: 'https://barnbarn96.github.io/frontend/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
 }
